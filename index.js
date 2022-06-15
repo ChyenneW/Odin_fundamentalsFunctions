7cda13a339a708e1622ab38aa6ed8132780ebe0f
ChyenneW <williamscheyenne771@gmail.com>
2022-06-15 12:52:21 -0500

    Create a function that capitalizes the first letter

--- index.js
+++ index.js
@@ -7,3 +7,20 @@ function multiply(a, b) {
   let theProduct = a * b;
   console.log(theProduct);
 }
+
+function capitalize(string) {
+  /* Make whole string lowercased */
+  let smallString = string.toLowerCase();
+
+  /* Capitalize first letter */
+  let firstLetter = smallString.charAt(0).toUpperCase();
+  smallString.splice(1);
+
+  let fixedString = firstLetter + smallString;
+  console.log(fixedString);
+}
+
+function lastLetter(string) {
+  let lastLetter = string.charAt(string.length - 1);
+  console.log(lastLetter);
+}
