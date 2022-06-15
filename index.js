998017c5e3c47f9220257a533c598c159345543d
ChyenneW <williamscheyenne771@gmail.com>
2022-06-15 13:10:47 -0500

    Fix bug in capitalize

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ function add7(a) {
   let sum = a + 7;
   console.log(sum);
 }
+add7(4);
 
 function multiply(a, b) {
   let theProduct = a * b;
   console.log(theProduct);
 }
+multiply(2, 8);
 
 function capitalize(string) {
   /* Make whole string lowercased */
@@ -14,13 +16,14 @@ function capitalize(string) {
 
   /* Capitalize first letter */
   let firstLetter = smallString.charAt(0).toUpperCase();
-  smallString.splice(1);
 
-  let fixedString = firstLetter + smallString;
+  let fixedString = firstLetter + smallString.slice(1);
   console.log(fixedString);
 }
+capitalize("hello");
 
 function lastLetter(string) {
   let lastLetter = string.charAt(string.length - 1);
   console.log(lastLetter);
 }
+lastLetter("abcde");
